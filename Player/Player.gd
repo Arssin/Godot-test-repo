b4e65c01b94b7ced10b00394207d8c0192a773b2
Mateusz Binięda <mat.binieda@gmail.com>
2023-07-09 16:13:33 +0200

    add camera following player

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,12 +2,22 @@ extends CharacterBody2D
 
 
 @export var speed = 300
+@onready var anim = $AnimatedSprite2D
 
+func _ready():
+	anim.play("Idle")
 
 
 func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right","move_up", "move_down")
-	velocity = input_direction * speed
+	
+	if input_direction:
+		velocity = input_direction * speed
+		anim.play("Run")
+	else: 
+		velocity = input_direction * 0
+		anim.play("Idle")
+
 
 
 func _process(delta):
