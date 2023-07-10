a02d8713e2596ceb6ea503f498a2a07e39f33004
Mateusz Binięda <mat.binieda@gmail.com>
2023-07-10 23:30:15 +0200

    player position check

--- EnemiesScene/Slime.gd
+++ EnemiesScene/Slime.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
+var player 
 
 
 func _on_player_detection_body_entered(body):
 	if body.name == "Player":
-		print("Player")
+		player = $"../../Player/Player"
+		print(player.global_position)
