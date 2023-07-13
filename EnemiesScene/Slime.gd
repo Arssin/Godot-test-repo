73284e98dc127feff764c90464527df5ead1d784
Mateusz Binięda <mat.binieda@gmail.com>
2023-07-14 00:10:03 +0200

    Slime animation add

--- EnemiesScene/Slime.gd
+++ EnemiesScene/Slime.gd
@@ -8,10 +8,21 @@ func _physics_process(delta):
 	if chase_player:
 		velocity = (player.position - position).normalized() * speed
 		move_and_collide(velocity * delta)
+		$AnimatedSprite2D.play('Move')
+		
+		if(player.position.x - position.x) < 0:
+			$AnimatedSprite2D.flip_h = false
+		else:
+			$AnimatedSprite2D.flip_h = true
+			
+	else:
+		$AnimatedSprite2D.play('Idle')
+
 
 func _on_player_detection_body_entered(body):
 	player = body
 	chase_player = true
+	
 
 
 func _on_player_detection_body_exited(body):
