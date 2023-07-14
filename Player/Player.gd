6246930759f6f6ad272f6ab319c58dd89a0635e2
Mateusz Binięda <mat.binieda@gmail.com>
2023-07-15 01:41:49 +0200

    add rotation with mouse

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,12 +8,12 @@ extends CharacterBody2D
 
 func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right","move_up", "move_down")
-
+	var position_mouse_x = get_global_mouse_position().x
 	
-	if input_direction[0] == -1:
+	if position_mouse_x < global_position.x:
 		$Sprites.scale.x = -1
-#		$Sprites/AnimatedSprite2D.flip_h = true
-	elif input_direction[0] == 1: 
+#		$Sprites/AnimatedSprite2D.flip_h = trues
+	elif position_mouse_x > global_position.x: 
 		$Sprites.scale.x = 1
 #		$Sprites/AnimatedSprite2D.flip_h = false
 	if input_direction:
@@ -29,5 +29,7 @@ func _process(delta):
 	get_input()
 	move_and_slide()
 
-	
+
+func _unhandled_input(event):
+	pass
 
