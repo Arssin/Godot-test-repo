8111c9f8512898f6da7807437f6d14adeec87246
Mateusz Binięda <mat.binieda@gmail.com>
2023-07-15 19:22:48 +0200

    add global script

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -6,6 +6,8 @@ signal player_fired_attack(range_attack, position, direction)
 
 @export var Range_attack: PackedScene
 
+var player_is_attacking
+
 
 @onready var anim = $AnimationPlayer
 @onready var wand_attack_point = $Sprites/Wand_attack_point
@@ -17,12 +19,17 @@ signal player_fired_attack(range_attack, position, direction)
 func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right","move_up", "move_down")
 	var position_mouse_x = get_global_mouse_position().x
+
+#	print(player_is_attacking)
 	
 	if position_mouse_x < global_position.x:
 		$Sprites.scale.x = -1
 	elif position_mouse_x > global_position.x: 
 		$Sprites.scale.x = 1
-	if input_direction:
+		
+	if player_is_attacking:
+		anim.play("Attack")
+	elif input_direction:
 		velocity = input_direction * player_stats.speed
 		anim.play("Run")
 	else: 
@@ -38,16 +45,23 @@ func _process(delta):
 
 func _unhandled_input(event):
 	if event.is_action_pressed("attack_mouse"):
+#		player_is_attacking = true
 		attack_range()
+		
 		
 
 func attack_range():
 	if attack_cd.is_stopped():
+#		if player_is_attacking:
+#			player_is_attacking = false
+			
 		var bullet_instance = Range_attack.instantiate()
 		var target = get_global_mouse_position()
 		var direction_to_mouse = wand_attack_point.global_position.direction_to(target).normalized()
 		emit_signal("player_fired_attack", bullet_instance, wand_attack_point.global_position, direction_to_mouse)
 		attack_cd.start()
+		
+
 
 
 func handle_hit():
