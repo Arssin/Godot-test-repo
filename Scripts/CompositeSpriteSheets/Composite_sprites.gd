62b4d463e785ee9fdd7b602e7939b6b06c335ae8
Mateusz Binięda <mat.binieda@gmail.com>
2023-08-02 22:38:34 +0200

    composite sprites preload

---
+++ Scripts/CompositeSpriteSheets/Composite_sprites.gd
@@ -0,0 +1,28 @@
+extends Node
+
+const body_spritesheet = {
+	0: ["Body", preload("res://CompositeSpriteSheets/Body.png")]
+}
+
+const shadow_spritesheet = {
+	0: ["Shadow", preload("res://CompositeSpriteSheets/shadow.png")]
+}
+
+const hands_spritesheet = {
+	0: ["Hands", preload("res://CompositeSpriteSheets/Hands.png")]
+}
+
+const head_spritesheet = {
+	0: ["Standard hair", preload("res://CompositeSpriteSheets/Head/head_standard_hair.png")],
+	1: ["Leather helmet", preload("res://CompositeSpriteSheets/Head/head_leather_helmet.png")]
+}
+
+const armor_spritesheet = {
+	0: ["Standard armor", preload("res://CompositeSpriteSheets/Body/body_main.png")],
+	1: ["Leather armor", preload("res://CompositeSpriteSheets/Body/body_leather_armor.png")]
+}
+
+const lowerBody_spritesheet = {
+	0: ["Standard pants", preload("res://CompositeSpriteSheets/LowerBody/lowerbody_standard.png")],
+	1: ["Leather pants", preload("res://CompositeSpriteSheets/LowerBody/lowerbody_leather_pants.png")]
+}
