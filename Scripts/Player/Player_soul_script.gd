686b511d4d33fe4b7936258998bed657e5d27a52
Mateusz Binięda <mat.binieda@gmail.com>
2023-09-16 18:59:23 +0200

    add player soul script

---
+++ Scripts/Player/Player_soul_script.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	get_input()
+	
+	
+	
+
+func get_input():
+	var input_direction = Input.get_vector("move_left", "move_right","move_up", "move_down")
+	var position_mouse_x = get_global_mouse_position().x
+	
+	if position_mouse_x < global_position.x:
+		pass
+	elif position_mouse_x > global_position.x: 
+		pass
