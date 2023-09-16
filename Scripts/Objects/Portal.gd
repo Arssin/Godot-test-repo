afd3d9c4cc0991669fb9e74186755c5dcb32f81d
Mateusz Binięda <mat.binieda@gmail.com>
2023-09-17 00:08:35 +0200

    add handling teleport

--- Scripts/Objects/Portal.gd
+++ Scripts/Objects/Portal.gd
@@ -1,13 +1,25 @@
-extends AnimatedSprite2D
+extends CharacterBody2D
 
 
 @onready var _animated_sprite = $Portal_sprite
 
 
+var player_entered = false
+
 func _process(delta):
 	_animated_sprite.play("portal-animation")
+	
+	if player_entered == true:
+		print('essa')
 	
 
 
 func _on_area_2d_body_entered(body):
-	pass # Replace with function body.
+	if body.has_method('get_input_soul_player'):
+		player_entered = true
+	
+
+
+func _on_area_2d_body_exited(body):
+	if body.has_method("get_input_soul_player"):
+		player_entered = false
--- Scripts/Player/Player_soul_script.gd
+++ Scripts/Player/Player_soul_script.gd
@@ -10,13 +10,13 @@ func _ready():
 
 
 func _process(delta):
-	get_input()
+	get_input_soul_player()
 	move_and_slide()
 	
 	
 	
 
-func get_input():
+func get_input_soul_player():
 	var input_direction = Input.get_vector("move_left", "move_right","move_up", "move_down")
 	var position_mouse_x = get_global_mouse_position().x
 	
@@ -30,3 +30,7 @@ func get_input():
 		velocity = input_direction * player_stats.player_speed
 	else: 
 		velocity = input_direction * 0
+		
+
+
+
