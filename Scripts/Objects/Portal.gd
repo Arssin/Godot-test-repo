e898d6abb433b90ff42aa38a20be44ec31d75bb7
Mateusz Binięda <mat.binieda@gmail.com>
2023-09-16 23:33:17 +0200

    changes to teleportation

--- Scripts/Objects/Portal.gd
+++ Scripts/Objects/Portal.gd
@@ -1,9 +1,13 @@
 extends AnimatedSprite2D
 
 
-@onready var _animated_sprite = $"."
+@onready var _animated_sprite = $Portal_sprite
 
 
 func _process(delta):
 	_animated_sprite.play("portal-animation")
 	
+
+
+func _on_area_2d_body_entered(body):
+	pass # Replace with function body.
